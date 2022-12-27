76bbdf4c8ba731edb92978faebdb5dab2df9383f
Alexandr <kb_299@bk.ru>
2022-12-27 16:22:08 +0300

    HomWork 8 дополнения

--- Spring_Boot_and_Hibernate/src/main/java/ru/gb/spring_boot_and_hibernate/repository/ProductRepository.java
+++ Spring_Boot_and_Hibernate/src/main/java/ru/gb/spring_boot_and_hibernate/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package ru.gb.spring_boot_and_hibernate.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import ru.gb.spring_boot_and_hibernate.model.Product;
 
@@ -10,4 +12,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> searchAllByTitleContainingAndPriceGreaterThan(String value, Double min);
     List<Product> searchAllByTitleContainingAndPriceGreaterThanAndPriceLessThan(String value, Double min, Double max);
+
+/*    //Пример создания кастомного запроса с параметрами
+    @Transactional
+    @Modifying
+    @Query("UPDATE Product p set p.price = ?2 where p.id = ?1")
+    void changePrice(Long id, Double price);*/
 }
