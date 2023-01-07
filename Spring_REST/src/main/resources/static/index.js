75f5aa9473f200d6b80b955a205dc968484ad265
Alexander <100999225+AlexanderNaide@users.noreply.github.com>
2023-01-07 18:10:41 +0300

    Spring_Rest - привязали JsonView

--- Spring_REST/src/main/resources/static/index.js
+++ Spring_REST/src/main/resources/static/index.js
@@ -14,9 +14,9 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
             bF = true;
             min = $scope.filter.min !== null ? $scope.filter.min : null;
             max = $scope.filter.max !== null ? $scope.filter.max : null;
-            cat = $scope.filter.cat !== "Все" ? $scope.filter.cat : null;
-            sub_cat = $scope.filter.sub_cat !== "Все" ? $scope.filter.sub_cat : null;
-            man = $scope.filter.man !== "Все" ? $scope.filter.man : null;
+            cat = $scope.filter.cat !== "пїЅпїЅпїЅ" ? $scope.filter.cat : null;
+            sub_cat = $scope.filter.sub_cat !== "пїЅпїЅпїЅ" ? $scope.filter.sub_cat : null;
+            man = $scope.filter.man !== "пїЅпїЅпїЅ" ? $scope.filter.man : null;
         }
         $http({
             url: contextPath,
@@ -76,8 +76,8 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
         let cat;
         let sub_cat;
         if ($scope.filter !== null){
-            cat = $scope.filter.cat !== "Все" ? $scope.filter.cat : null;
-            sub_cat = $scope.filter.sub_cat !== "Все" ? $scope.filter.sub_cat : null;
+            cat = $scope.filter.cat !== "пїЅпїЅпїЅ" ? $scope.filter.cat : null;
+            sub_cat = $scope.filter.sub_cat !== "пїЅпїЅпїЅ" ? $scope.filter.sub_cat : null;
         }
         $http({
             url: contextPath + "/man",
